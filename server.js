caa021ded8a1ca6b9fd9901f2ea0237cd272ebd8
Your_Name <joseph161216@gmail.com>
2020-10-17 19:49:44 -0700

    add initial code

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to mongodb
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+// Use routes
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/users", require("./routes/api/users"));
+// app.use("/api/expenses", require("./routes/api/expense"));
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+
+// start server
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
