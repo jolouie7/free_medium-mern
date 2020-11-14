6363049783b47ad68ca5360e1e0ab93030734032
Your_Name <joseph161216@gmail.com>
2020-11-13 19:26:01 -0800

    use slugify lib and add a random id at the end to make it more unique

--- models/Article.js
+++ models/Article.js
@@ -1,5 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const slugify = require('slugify')
+const nanoid = require('nanoid')
+const marked = require("marked");
+const createDOMPurify = require("dompurify");
+const { JSDOM } = require("jsdom");
+const window = new JSDOM("").window;
+const DOMPurify = createDOMPurify(window);
+// Instead of a custom slugify function, we can use the library slugify
+
+// const clean = DOMPurify.sanitize(dirty);
 
 // Create Schema
 const ArticleSchema = new Schema(
@@ -29,27 +39,35 @@ const ArticleSchema = new Schema(
       type: Schema.Types.ObjectId,
       ref: "User",
     },
-    slug: { type: String },
+    slug: { type: String, unique: true },
+    sanitizedHtml: {
+      type: String,
+      required: true
+    }
   },
   {
     timestamps: true,
   }
 );
 
-function slugify(text) {
-  return text
-    .toString()
-    .toLowerCase()
-    .replace(/\s+/g, "-") // replace spaces with -
-    .replace(/[^\w\-]+/g, "") // remove all non-word chars
-    .replace(/\-\-+/g, "-") // replace multiple - with single -
-    .replace(/^-+/, "") // trim - from start of text
-    .replace(/-+$/, ""); // trim - from end of text
-}
+// ! not using anymore
+// function slugify(text) {
+//   return text
+//     .toString()
+//     .toLowerCase()
+//     .replace(/\s+/g, "-") // replace spaces with -
+//     .replace(/[^\w\-]+/g, "") // remove all non-word chars
+//     .replace(/\-\-+/g, "-") // replace multiple - with single -
+//     .replace(/^-+/, "") // trim - from start of text
+//     .replace(/-+$/, ""); // trim - from end of text
+// }
 
 // make sure that the slug is created from the title
 ArticleSchema.pre("save", function(next) {
-  this.slug = slugify(this.title);
+  if (this.title) {
+    this.slug = slugify(this.title, { lower: true, strict: true }) + `-${nanoid(10)}`;
+  }
+
   next();
 })
 
